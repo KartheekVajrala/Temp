4f47475fbe695add79b7d3b7fbd03839723e6958
KartheekVajrala <48596971+KartheekVajrala@users.noreply.github.com>
2020-12-14 14:40:50 +0530

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -1,10 +1,10 @@
 class Agent:
   """Class for declaring Agent Object
   Takes 3 params
-  name and Roll Number
-  currentYear : currently studying year
+  dep : department
+  year and hall currently in.
   """
-  def __init__(self, name, RollNo, currentYear):
-    self.name = name
-    self.RollNo = RollNo
-    self.currentYear = currentYear
+  def __init__(self, dep, year, hall):
+    self.dep = dep
+    self.year = year
+    self.hall = hall
